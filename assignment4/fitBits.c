3a6f39e0f0de30a22bd522c0594f46a1fc325fb3
Garima <loombagarima33@gmail.com>
2026-08-12 13:54:01 +0000

    Updated fitBits solution

--- assignment4/fitBits.c
+++ assignment4/fitBits.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int fitsBits(int x, int n) {
-    int shift = 32 + (~n + 1);
-    return !((x << shift) >> shift ^ x);
+    int shift = x << (32-n);
+    int right = shift >> (32-n);
+    return !(shift ^right);
+
 }
 
 int main() {
     printf("fitsBits(5, 3) = %d\n", fitsBits(5, 3));
-    printf("fitsBits(-4, 3) = %d\n", fitsBits(-4, 3));
+    printf("fitBits(-4,3) = %d\n" , fitsBits(-4 ,3));
    
 
     return 0;

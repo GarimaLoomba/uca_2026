6b0d8deeec3e5bc828f21e14433c2621d4655305
GarimaLoomba <loombagarima33@gmail.com>
2026-09-18 08:01:33 +0000

    Added conditional function

---
+++ assignment4/conditional.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+
+
+int conditional(int x, int y, int z) {
+    int mask = ~(!x) + 1;
+    return (mask & z) | (~mask & y);
+}
+
+int main() {
+    printf("conditional(2, 4, 5) = %d\n", conditional(2, 4, 5));
+    printf("conditional(0, 4, 5) = %d\n", conditional(0, 4, 5));
+    printf("conditional(-3, 10, 20) = %d\n", conditional(-3, 10, 20));
+
+    return 0;
+}
+
+
